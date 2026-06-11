d450a55138a45d59067c2c840561c192f548403b
Uday Bhaskar <2503739@cognizant.com>
2026-06-11 08:58:25 +0530

    FEAT: Swagger Configuration

--- src/main/java/com/project/flightOps/FlightOpsApplication.java
+++ src/main/java/com/project/flightOps/FlightOpsApplication.java
@@ -1,9 +1,17 @@
 package com.project.flightOps;
 
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        scheme = "bearer"
+)
 public class FlightOpsApplication {
 
 	public static void main(String[] args) {
